10344be07594d543f78bc7e5e1314c16b3ec4105
J4viscript <tezjavi@gmail.com>
2023-02-17 15:22:41 -0700

    Initial Commit

---
+++ Design/DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignApp.swift
+//  Design
+//
+//  Created by Javier Oskar Murillo Cota on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
